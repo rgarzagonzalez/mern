f0ed3e7e76dc4afba85b5acf9c8bb5553ce5e99b
Ricardo Garza <ricardo@madhouse.com.mx>
2025-03-25 09:16:09 -0600

    Frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,52 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Record from './components/Record';
+import RecordList from './components/RecordList';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    chiuldren: [
+      {
+        path: '/',
+        element: <RecordList />,
+      },
+    ],
+  },
+
+  {
+    path: 'create',
+    element: <App />,
+    children: [
+      {
+        path: '/edit/:id',
+        element: <Record />,
+      },
+    ],
+  },
+
+  {
+    path: '/create',
+    element: <App />,
+    children: [
+      {
+        path: '/create',
+        element: <Record />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router ={router} />
+  </React.StrictMode>
+);
